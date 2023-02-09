eee3304f37869316b725c85fa32e166702940a4e
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-02-09 08:06:03 +0300

    Complete Assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,8 @@
 class Shoe
+  #add attributes
    attr_reader :brand
    attr_accessor :color, :size, :material, :condition
-
+  #initialize with a brand
     def initialize(brand)
       @brand = brand
    end
